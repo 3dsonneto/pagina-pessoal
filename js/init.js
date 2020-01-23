747e64aaa573a56a54562df7e6e9326668ac92b6
3dsonneto <edson_neto1996@hotmail.com>
2020-01-22 21:52:45 -0300

    adicionando o firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCE7o-FGh_PtywjRkXEbRwoPiWQy1AyD4g",
+  authDomain: "linkme-26841.firebaseapp.com",
+  databaseURL: "https://linkme-26841.firebaseio.com",
+  projectId: "linkme-26841",
+  storageBucket: "linkme-26841.appspot.com",
+  messagingSenderId: "896683467781",
+  appId: "1:896683467781:web:3eacb63545722783aba86c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
